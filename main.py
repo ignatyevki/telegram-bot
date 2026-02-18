06c8fe7a832c459f09ffafeca2fff24d902520b1
ignatyevki <ignatyevki@gmail.com>
2026-02-18 15:11:33 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv(TOKEN)
+TOKEN = os.getenv("TOKEN")
 
-async def start(update Update, context ContextTypes.DEFAULT_TYPE)
-    await update.message.reply_text(Бот працює на Render 🚀)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Бот працює на Render 🚀")
 
 app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler(start, start))
+app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
